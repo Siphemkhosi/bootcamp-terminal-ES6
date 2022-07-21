77160b5c82554ef785b9f11312d733899b2f10f3
SipheMkhosi <siphemkhosi7@gmail.com>
2022-07-21 11:46:52 +0200

    removed node_modules

--- totalPhoneBill.js
+++ totalPhoneBill.js
@@ -12,7 +12,7 @@ export default function totalPhoneBill(billCost){
      //console.log(callCost)
     }
      var totalCost = callCost + smsCost;
-     console.log(totalCost)
+    
     }
      return "R" + totalCost.toFixed(2);
     }
